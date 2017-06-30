7fd1b2d46feb44abb72031772a5d7d85489f518a
kitet <kitetnet@gmail.com>
2017-06-30 16:00:14 +0300

    added submit button on the index form to capture user input

--- app.rb
+++ app.rb
@@ -4,5 +4,8 @@ require './lib/counter'
 also_reload('lib/**/*.rb')
 
 get('/') do
+	@wordn=params.fetch('word')
+	@stringn=params.fetch('string')
+	#@results= CountOccurence.countWord(word, string)
 	erb(:index)
 end
