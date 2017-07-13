f1c47fe886a792886f7234e15f6adb239681e88f
kitet <kitetnet@gmail.com>
2017-07-13 08:49:51 +0300

    fixed error on the logic class

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,25 +1,18 @@
 class CountOccurence
 	def countWord(word,string,selectedvalue)
-		# if word.length>(0) && string.length>(0)
 			noofoccurence=0
-			tocheck=Regexp.new(word)
+			tocheck=word
 			string.gsub(/\s+/, " ")
 			string.split(" ").each do |item|
-				#item=item.gsub(/\!/,"")
 				if item=~/\!/
-					#do nothing
 				else
 					if selectedvalue.eql?(0)
-						item=~/\b#{tocheck}\b/ ? noofoccurence=+1: "do not match"
+						item=~/\b#{tocheck}\b/ ? noofoccurence+=1: "do not match"
 					else
-						item=~/#{tocheck}/ ? noofoccurence=+1: "do not match"
+						item=~/#{tocheck}/ ? noofoccurence+=1: "do not match"
 					end
 				end
 			end
 			noofoccurence
-		# else
-		# 	raise "Error"
-		# end
 	end
 end
-
